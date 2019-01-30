004e81c963af7e9d786ccc5eb2168e69d39d48f2
Ivan <iwalulya@yahoo.co.uk>
2019-01-30 22:56:53 +0100

    adding barcode

--- src/android/Printer.java
+++ src/android/Printer.java
@@ -771,8 +771,9 @@ public class Printer extends CordovaPlugin implements PrinterObserver{
         try {
             escCmd.append(escCmd.getBarcodeCmd(barcodeType, barcodeSetting, barcodeContent));
             escCmd.append(escCmd.getBarcodeCmd(Enum.valueOf(BarcodeType.class, "EAN13"), barcodeSetting, "123456789"));
-            escCmd.append(escCmd.getBarcodeCmd(Enum.valueOf(BarcodeType.class, "CODE93"), barcodeSetting, "987654321"));
-            escCmd.append(escCmd.getBarcodeCmd(Enum.valueOf(BarcodeType.class, "UPC_A"), barcodeSetting, "012909120912"));
+            escCmd.append(escCmd.getBarcodeCmd(Enum.valueOf(BarcodeType.class, "CODE39"), barcodeSetting, "987654321"));
+            escCmd.append(escCmd.getBarcodeCmd(Enum.valueOf(BarcodeType.class, "UPC_A"), barcodeSetting, "12909120912"));
+            escCmd.append(escCmd.getBarcodeCmd(Enum.valueOf(BarcodeType.class, "ITF"), barcodeSetting, "12345678901234"));
         } catch (SdkException e) {
             e.printStackTrace();
         }
